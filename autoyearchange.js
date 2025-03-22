be33e78e63e87dc517df64bf66929732d72fef63
Thanvir Alam Ripon <thanvirhossain50@gmail.com>
2025-03-22 19:32:06 +0600

    Add files via upload

---
+++ autoyearchange.js
@@ -0,0 +1,5 @@
+ // Get the current year
+ const currentYear = new Date().getFullYear();
+        
+ // Set the current year inside the element with id 'year'
+ document.getElementById("year").textContent = currentYear;
